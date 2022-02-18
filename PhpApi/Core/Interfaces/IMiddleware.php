244e1b8915f5eaaba1005b20721caf12e7407d39
Ismail Aatif <techcyber598@gmail.com>
2022-02-18 15:10:44 +0000

    Changed IMiddleware interface

--- PhpApi/Core/Interfaces/IMiddleware.php
+++ PhpApi/Core/Interfaces/IMiddleware.php
@@ -2,11 +2,20 @@
 
 namespace PhpApi\Core\Interfaces;
 
+use PhpApi\Core\Functions\Request;
+use PhpApi\Core\Functions\Response;
+
 /**
  * The middleware interface.
  */
 interface IMiddleware
 {
-    /** implement the middleware. */
-    public function use();
+
+    /**
+     * apply middleware to the context request. 
+     *
+     * @param Request $req the context request. 
+     * @param Response $res the context response. 
+     */
+    public function intercept(Request $req, Response $res);
 }
