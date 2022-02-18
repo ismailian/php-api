e027d9fa543bf931441548b61edc4b9fa3c83a69
Ismail Aatif <techcyber598@gmail.com>
2022-02-18 15:13:07 +0000

    Added Authorization functionality

---
+++ PhpApi/Middlewares/Authorization.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace PhpApi\Middlewares;
+
+use PhpApi\Core\Functions\Authenticator;
+use PhpApi\Core\Functions\Request;
+use PhpApi\Core\Functions\Response;
+use PhpApi\Core\Helpers\Middleware;
+
+/**
+ * The middleware class.
+ * monitors the authentication flow through the authorization header.
+ */
+class Authorization extends Middleware
+{
+
+    /**
+     * apply middleware to the context request. 
+     *
+     * @param Request $req the content request. 
+     */
+    public function intercept(Request $req, Response $res)
+    {
+        $this->auth = new Authenticator();
+        $this->allowed = $this->auth->check($req);
+    }
+}
