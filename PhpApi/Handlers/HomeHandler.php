0d3d496b74dad9c37732714c9d50e8d1100db60e
Ismail Aatif <techcyber598@gmail.com>
2022-02-24 11:03:36 +0000

    Added new Method

--- PhpApi/Handlers/HomeHandler.php
+++ PhpApi/Handlers/HomeHandler.php
@@ -2,6 +2,7 @@
 
 namespace PhpApi\Handlers;
 
+use PhpApi\Core\Functions\Database;
 use PhpApi\Core\Helpers\Handler;
 use PhpApi\Core\Functions\Request;
 use PhpApi\Core\Functions\Response;
@@ -24,6 +25,20 @@ class HomeHandler extends Handler
             'data' => [
                 'route' => $req->route,
             ]
+        ]);
+    }
+
+    /**
+     * 
+     * @param Request $req the current request
+     * @param Response $res the current response
+     */
+    public function Data(Request $req, Response $res)
+    {
+        $data = Database::instance()->limit(3)->select('wheels');
+        $res->status(200)->json([
+            'status' => true,
+            'data' => $data
         ]);
     }
 }
